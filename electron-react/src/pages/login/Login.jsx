6944e49a951a34b84392237fa5f2f6902fd618ce
majdifkih <majdifkih123@gmail.com>
2022-05-26 16:30:11 +0100

    commit

--- electron-react/src/pages/login/Login.jsx
+++ electron-react/src/pages/login/Login.jsx
@@ -2,6 +2,9 @@ import "./login.scss"
 import {useState} from 'react'
 import stock from './stock0.jpg'
 import logo from './logo.png'
+import LockIcon from '@mui/icons-material/Lock';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import EmailIcon from '@mui/icons-material/Email';
 function Login () {
   const [pwd, setPwd] = useState('');
   const [isRevealPwd, setIsRevealPwd] = useState(false);
@@ -22,17 +25,17 @@ function Login () {
               <td><div class="email">
                 
                 <div class="sec-2">
-                  <ion-icon name="mail-outline"></ion-icon>
+                <div className="img"><EmailIcon fontSize="small" collor="disabed"/></div>
                   <input type="email" name="email" placeholder="Username@gmail.com"/>
                 </div>
               </div></td>
               <td><div class="password">
                 <div class="sec-2">
-                  <ion-icon name="lock-closed-outline"></ion-icon>
+                  <div className="img"><LockIcon fontSize="small" collor="disabed"/></div>
                   <input class="pas" id ="myInput" type={isRevealPwd ? "text" : "password"}
           value={pwd}
           onChange={e => setPwd(e.target.value)} name="password" placeholder="············"/>
-                  <ion-icon class="show-hide" name="eye-outline" onClick={() => setIsRevealPwd(prevState => !prevState)}></ion-icon>
+                  <div className="img" onClick={() => setIsRevealPwd(prevState => !prevState)}><VisibilityIcon fontSize="small" collor="disabed"/></div>
                 </div>
               </div></td>
             </tr>

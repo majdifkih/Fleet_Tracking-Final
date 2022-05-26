fd9f6664a3f5ebcc4fac031f38065587f7bb5d22
majdifkih <majdifkih123@gmail.com>
2022-05-27 00:39:55 +0100

    commit

--- electron-react/src/components/sidebar/Sidebar.jsx
+++ electron-react/src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,14 @@ import user from './user.jpg'
 import user1 from './user1.jpg'
 import setting from './setting.jpg'
 import logout from './logout.jpg'
-
+import HomeIcon from '@mui/icons-material/Home';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import StoreIcon from '@mui/icons-material/Store';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import GroupIcon from '@mui/icons-material/Group';
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
+import LogoutIcon from '@mui/icons-material/Logout';
 const Sidebar = () => {
  
   return (
@@ -30,85 +37,45 @@ const Sidebar = () => {
             <br />
             <li>
             <Link to="/" >
-            <img
-              src={clock}
-              className="circle"
-              width="20"
-              height="20"
-              alt=""/> Accueil
+            <HomeIcon className="iconlist" fontSize="small"/> Accueil
                 </Link>
             </li>
             <li>
             <Link to="/devicelist" >
-            <img
-              src={watch}
-              className="circle"
-              width="20"
-              height="20"
-              alt=""/> Fleet and Devices
+           <LocalShippingIcon className="iconlist" fontSize="small"/> Fleet and Devices
                 </Link>
             </li>
             <li>
             <Link to="/fleet">
                    
-            <img
-              src={gps}
-              className="circle"
-              width="20"
-              height="20"
-              alt=""/> Fleet Tracking</Link>
+            <GpsFixedIcon className="iconlist" fontSize="small"/> Fleet Tracking</Link>
                
             </li>
             <li>
             <Link to="/store" >
-            <img
-              src={user}
-              className="circle"
-              width="17"
-              height="17"
-              alt=""/> Stores
+            <StoreIcon className="iconlist" fontSize="small"/> Stores
                 </Link>
             </li>
             <li>
             <Link to="/inventory" >
-            <img
-              src={setting}
-              className="circle"
-              width="17"
-              height="17"
-              alt=""/> Inventory
+            <SettingsIcon className="iconlist" fontSize="small"/> Inventory
                 </Link>
             </li>
             <li>
               <Link to="/storetable" >
-              <img
-              src={user}
-              className="circle"
-              width="17"
-              height="17"
-              alt=""/> Clients
+              <GroupIcon className="iconlist" fontSize="small"/> Clients
               </Link>
             </li>
             <li>
            
               <Link to="/deviceinfo" >
-              <img
-              src={user1}
-              className="circle"
-              width="17"
-              height="17"
-              alt=""/> Users
+              <AccountCircleIcon className="iconlist" fontSize="small"/> Users
                 </Link>
             </li>
            
             <li>
             <Link to="/logout" >
-            <img
-              src={logout}
-              className="circle"
-              width="20"
-              height="20"
-              alt=""/> Log out
+            <LogoutIcon className="iconlist" fontSize="small"/> Log out
               </Link>
             </li>
           </ul>
